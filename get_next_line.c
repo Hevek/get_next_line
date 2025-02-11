d11147b386f6dbebaa28d3830a4274d35dccd310
restevez <restevez@student.42sp.org.br>
2025-02-11 19:48:02 -0300

    GNL - Day Eight - v0

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: restevez <restevez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/11 19:45:25 by restevez          #+#    #+#             */
-/*   Updated: 2025/02/11 19:45:27 by restevez         ###   ########.fr       */
+/*   Created: 2025/01/12 07:18:07 by restevez          #+#    #+#             */
+/*   Updated: 2025/02/11 19:47:24 by restevez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,73 +14,103 @@
 
 char	*get_next_line(int fd);
 
-/* int	main(void)
+/* #include <stdio.h>
+int	main(int argc, char *argv[])
 {
 	char	*line;
 	int		fd;
 	int		n;
 	char	test;
+	size_t	end;
 
-	n = 1;
-	test = '1';
-	if (test == '1')
+	end = 1;
+	if (argc < 1)
+		return (1);
+	while (end)
 	{
+		n = 1;
+		test = '0' + end;
+		if (argc == 2)
+			test = argv[1][0];
+		printf("=======Test %c=======\n\n", test);
+		if (test == '1')
+		{
+			fd = open(
+	"/home/hevek/francinette/tests/get_next_line/fsoares/read_error.txt",
+					O_RDONLY);
+			line = get_next_line(fd);
+			printf("Line %d: %s", n++, line);
+			free(line);
+			line = get_next_line(fd);
+			printf("Line %d: %s", n++, line);
+			free(line);
+			if (BUFFER_SIZE > 100) {
+				char *temp;
+				do {
+					temp = get_next_line(fd);
+					free(temp);
+				} while (temp != NULL);
+			}
+			line = get_next_line(fd);
+			printf("Line %d: %s", n++, line);
+			free(line);
+			close(fd);
+			end++;
+			printf("<- File Ended\n\n\n\n==END_OF_TEST==\n\n");
+			if (argc == 2)
+				return (0);
+			continue ;
+		}
+		else if (test == '2')
+		{
+			fd = open(
+	"/home/hevek/francinette/tests/get_next_line/fsoares/one_line_no_nl.txt",
+					O_RDONLY);
+		}
+		else if (test == '3')
+		{
+			fd = open(
+	"/home/hevek/francinette/tests/get_next_line/fsoares/multiple_nl.txt",
+					O_RDONLY);
+		}
+		else if (test == '4')
+		{
 		fd = open(
-"/nfs/homes/restevez/francinette/tests/get_next_line/fsoares/read_error.txt",
-				O_RDONLY);
-		line = get_next_line(fd);
-		printf("Line %d: %s", n++, line);
-		free(line);
-		line = get_next_line(fd);
-		printf("Line %d: %s", n++, line);
-		free(line);
-		if (BUFFER_SIZE > 100) {
-			char *temp;
-			do {
-				temp = get_next_line(fd);
-				free(temp);
-			} while (temp != NULL);
+	"/home/hevek/francinette/tests/get_next_line/fsoares/variable_nls.txt",
+					O_RDONLY);
+		}
+		else if (test == '5')
+		{
+		fd = open(
+	"/home/hevek/francinette/tests/get_next_line/fsoares/1char.txt",
+					O_RDONLY);
+		}
+		else
+		{
+			fd = open("test", O_RDONLY);
 		}
 		line = get_next_line(fd);
-		printf("Line %d: %s", n++, line);
+		while (line)
+		{
+			printf("Line %d: %s", n++, line);
+			free(line);
+			line = get_next_line(fd);
+		}
 		free(line);
+		if (test == '3')
+		{
+			line = get_next_line(fd);
+			printf("Line: %d: %s", n, line);
+		}
 		close(fd);
-		return (0);
-	}
-	else if (test == '2')
-	{
-		fd = open(
-"/nfs/homes/restevez/francinette/tests/get_next_line/fsoares/one_line_no_nl.txt",
-				O_RDONLY);
-	}
-	else if (test == '3')
-	{
-		fd = open(
-"/nfs/homes/restevez/francinette/tests/get_next_line/fsoares/multiple_nl.txt",
-				O_RDONLY);
-	}
-	else if (test == '4')
-	{
-	fd = open(
-"/nfs/homes/restevez/francinette/tests/get_next_line/fsoares/variable_nls.txt",
-				O_RDONLY);
-	}
-	else if (test == '5')
-	{
-	fd = open(
-"/nfs/homes/restevez/francinette/tests/get_next_line/fsoares/1char.txt",
-				O_RDONLY);
-	}
-	else
-	{
-		fd = open("test", O_RDONLY);
-	}
-	line = get_next_line(fd);
-	while (line)
-	{
-		printf("Line %d: %s", n++, line);
-		free(line);
-		line = get_next_line(fd);
+		if (end == 7)
+			break ;
+		end++;
+		if (line == NULL)
+			printf("<- File Ended\n");
+		printf("\n\n\n==END_OF_TEST==\n\n");
+		if (argc == 2)
+			return (0);
 	}
 	free(line);
 	close(fd);
@@ -96,8 +126,7 @@ char	*get_next_line(int fd)
 		return (cleanup_list(&list, 1), NULL);
 	line = fill_list(&list, fd);
 	if (!line)
-		return (cleanup_list(&list, 1), free(list), NULL);
-	cleanup_list(&list, 0);
+		return (cleanup_list(&list, 1), NULL);
 	return (line);
 }
 
@@ -137,7 +166,7 @@ void	append_str(t_str_list **list, char *str)
 		*list = new;
 	else if ((*list)->str == NULL)
 	{
-		free(*list);
+		cleanup_list(list, 0);
 		*list = new;
 	}
 	else
@@ -158,8 +187,10 @@ char	*transfer_line(t_str_list **list)
 	size_t		j;
 	t_str_list	*tmp;
 
+	if (!*list || !(*list)->str)
+		return (cleanup_list(list, 1), NULL);
 	line = ft_calloc(get_line_size(*list) + 1, 1);
-	if (!line || !*list || !(*list)->str)
+	if (!line)
 		return (free(line), cleanup_list(list, 1), NULL);
 	j = -1;
 	tmp = *list;
@@ -170,12 +201,10 @@ char	*transfer_line(t_str_list **list)
 		{
 			line[++j] = tmp->str[i];
 			if (tmp->str[i] == '\n')
-			{
-				return (line[++j] = '\0', line);
-			}
+				return (cleanup_list(list, 0), line[++j] = '\0', line);
 		}
 		tmp = tmp->next;
 	}
 	line[++j] = '\0';
-	return (line);
+	return (cleanup_list(list, 0), line);
 }
